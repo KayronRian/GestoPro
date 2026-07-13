9dcf938f4a61cdc8602046a1900e2043b0656791
KayronRian <kayrotabunito@gmail.com>
2026-07-13 18:26:53 -0300

    Update app_state.dart

--- gestopro/lib/services/app_state.dart
+++ gestopro/lib/services/app_state.dart
@@ -1,18 +1,24 @@
+// Lógica de serviço (Banco de Dados ou Estado)
+
 import 'package:flutter/material.dart';
 import '../models/models.dart';
 import 'db_service.dart';
 
+// classe central que gerencia o estado global do aplicativo.
 class AppState extends ChangeNotifier {
-  static final AppState _instance = AppState._();
+  static final AppState _instance = AppState._();   // Garante que o AppState seja acessado de qualquer lugar com a mesma instância.
   factory AppState() => _instance;
   AppState._();
 
+    // Instância do serviço de banco de dados para operações internas.
   final _db = DbService();
 
+    // --- Variáveis de Estado Privadas ---
   Usuario? _usuario;
   Empresa? _empresa;
   bool _loading = false;
 
+  // --- Getters Públicos ---
   Usuario? get usuario => _usuario;
   Empresa? get empresa => _empresa;
   bool get loading => _loading;
@@ -21,6 +27,8 @@ class AppState extends ChangeNotifier {
   String get empresaId => _usuario?.empresaId ?? '';
   String get usuarioNome => _usuario?.nome ?? '';
 
+    /// Inicializa o estado do app.
+  /// Verifica se já existe uma sessão salva localmente para logar automaticamente.
   Future<void> init() async {
     await _db.init();
     final u = await _db.getSessionUser();
@@ -31,6 +39,7 @@ class AppState extends ChangeNotifier {
     }
   }
 
+    /// Realiza a tentativa de login do usuário.
   Future<String?> login(String email, String senha) async {
     _loading = true;
     notifyListeners();
@@ -39,8 +48,8 @@ class AppState extends ChangeNotifier {
       if (u == null) return 'E-mail ou senha incorretos';
       _usuario = u;
       _empresa = await _db.getEmpresa(u.empresaId);
-      await _db.saveSession(u);
-      await _db.addLog(
+      await _db.saveSession(u); // Salva a sessão para que o usuário não precise logar de novo ao abrir o app.
+      await _db.addLog(       // Registra o login no histórico do sistema.
         empresaId: u.empresaId,
         usuarioNome: u.nome,
         acao: 'Login',
@@ -53,15 +62,17 @@ class AppState extends ChangeNotifier {
     }
   }
 
+   /// Finaliza a sessão do usuário e limpa os dados da memória e do armazenamento local.
   Future<void> logout() async {
     await _db.clearSession();
     _usuario = null;
     _empresa = null;
     notifyListeners();
   }
-
+  /// Verifica se o sistema já passou pela configuração inicial
   Future<bool> hasSetup() => _db.hasAdminSetup();
 
+    /// Recarrega as informações da empresa.
   Future<void> reloadEmpresa() async {
     if (_usuario != null) {
       _empresa = await _db.getEmpresa(_usuario!.empresaId);
